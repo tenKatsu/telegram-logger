ed4f17a1ac7da89b9ba8fc112cb585c1f1ea41ea
nyuszika7h <nyuszika7h@gmail.com>
2021-06-17 14:43:48 +0200

    Add basic edited message logging

--- telegram-logger.py
+++ telegram-logger.py
@@ -25,6 +25,8 @@ async def on_new_message(event):
         return
     chat_display = f'[{chat.username or chat.title} ({chat.id})]'
 
+    msg_display = f'({msg.id})'
+
     if msg.from_id:
         try:
             user = await client.get_entity(msg.from_id)
@@ -42,11 +44,49 @@ async def on_new_message(event):
 
     text = msg.message
 
-    out = f'{date} {chat_display}'
+    out = f'{date} {chat_display} MSG {msg_display}'
     if user_display:
         out += f' {user_display}'
     out += f' {text}'
     print(out)
 
+
+@client.on(events.MessageEdited)
+async def on_message_edited(event):
+    msg = event.message
+
+    date = msg.date.strftime('%Y-%m-%d %H:%M:%S')
+
+    chat = await client.get_entity(msg.peer_id)
+    if enabled_chats and chat.id not in enabled_chats:
+        return
+    if disabled_chats and chat.id in disabled_chats:
+        return
+    chat_display = f'[{chat.username or chat.title} ({chat.id})]'
+
+    msg_display = f'({msg.id})'
+
+    if msg.from_id:
+        try:
+            user = await client.get_entity(msg.from_id)
+        except ValueError:
+            await client.get_participants(chat.id)
+
+            try:
+                user = await client.get_entity(msg.from_id)
+            except ValueError:
+                await client.get_participants(chat.id, aggressive=True)
+                user = await client.get_entity(msg.from_id)
+        user_display = f'<{user.username} ({user.id})>'
+    else:
+        user_display = None
+
+    text = msg.message
+
+    out = f'{date} {chat_display} EDIT {msg_display}'
+    if user_display:
+        out += f' {user_display}'
+    out += f' {text}'
+    print(out)
 
 client.run_until_disconnected()
